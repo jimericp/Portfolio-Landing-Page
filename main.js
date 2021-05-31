ce316d04ee2ae2c89dc4d9db58d24439dd5410c9
Jimeric Pulido <pjimeric13@gmail.com>
2021-05-31 20:40:44 +0800

    Portfolio Landing Page

---
+++ main.js
@@ -0,0 +1,11 @@
+var options = {
+  animate: true,
+  patternWidth: 100,
+  patternHeight: 100,
+  grainOpacity: 0.06,
+  grainDensity: 1,
+  grainWidth: 1,
+  grainHeight: 1,
+};
+
+grained('#grained-bg', options);
